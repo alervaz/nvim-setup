e08c65ef3d2e27997328a71c0449dea6e1b53f5a
alervaz <alervaz2008@gmail.com>
2023-12-19 21:19:56 -0400

    updates

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,7 +2,7 @@ local lsp = require("lsp-zero")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
-local luasnip = require('luasnip')
+local ls = require('luasnip')
 
 
 local icons = {
@@ -169,6 +169,10 @@ lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
   lsp.default_keymaps({ buffer = bufnr })
   require('luasnip.loaders.from_vscode').load()
+  local s = ls.snippet
+  local t = ls.text_node
+  local i = ls.insert_node
+
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -197,7 +201,7 @@ lsp.on_attach(function(client, bufnr)
     },
     snippet = {
       expand = function(args)
-        luasnip.lsp_expand(args.body)
+        ls.lsp_expand(args.body)
       end,
     },
     formatting = {
@@ -220,7 +224,7 @@ lsp.format_on_save({
     ['templ'] = { 'templ' },
     ['lua_ls'] = { "lua" },
     ['html'] = { "html" },
-    ['null-ls'] = { "python", "go", "javascript", "typescript" },
+    ['null-ls'] = { "python", "go", "javascript", "typescript", "c" },
     ['elixirls'] = { "elixir" },
   }
 })
@@ -247,6 +251,7 @@ null_ls.setup({
     null_ls.builtins.formatting.goimports,
     null_ls.builtins.formatting.gofumpt,
     null_ls.builtins.formatting.golines,
+    null_ls.builtins.formatting.clang_format,
   }
 })
 
